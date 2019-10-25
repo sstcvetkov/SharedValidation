9e8b83e5f3bce3635d75b7e84c10beed7a305d64
Sergei Tcvetkov <sstcvetkov@gmail.com>
2019-10-25 22:18:56 +0400

    Add server project with DataAnnotations validation

---
+++ Server/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Server.Requests;
+
+namespace Server.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+		[HttpPost(nameof(Registration))]
+		public ActionResult Registration([FromBody]RegistrationRequest request)
+		{
+			return Ok($"{request.Name}, вы зарегистрированы!");
+		}
+	}
+}
---
+++ Server/Requests/RegistrationRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Requests
+{
+	public class RegistrationRequest
+	{
+		[StringLength(maximumLength: 50, MinimumLength = 2,
+			ErrorMessage = "Длина имени должна быть от 2 до 50 символов")]
+		[Required(ErrorMessage = "Требуется имя")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Требуется адрес эл. почты")]
+		[EmailAddress(ErrorMessage = "Некорректный адрес эл. почты")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Требуется пароль")]
+		[MaxLength(100, ErrorMessage = "{0} не может превышать {1} символов")]
+		[MinLength(6, ErrorMessage ="{0} должен быть минимум {1} символов")]
+		[DisplayName("Пароль")]
+		public string Password { get; set; }
+
+		[Required(ErrorMessage = "Требуется возраст")]
+		[Range(18,150, ErrorMessage = "Возраст должен быть в пределах от 18 до 150")]
+		public string Age { get; set; }
+
+		[DisplayName("Культура")]
+        public string Culture { get; set; }
+	}
+}
